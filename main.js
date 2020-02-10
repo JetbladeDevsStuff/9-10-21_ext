e835a7086fb272e28c3c2721504fff911d51021b
N <TerenceN@smsmd.org>
2020-02-10 17:33:32 -0500

    An unfineshed commit that focuses on shortening code and adding some bad code

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 // main.js of 9 + 10 = 21
+// This whole thing will wait a bit for jQuery to load
+setTimeout(() => {
+    var title = $("title").html();
+    // We will write a function that will check if the wanted tab title or 
+    // extention "on/off" state has been changed every 5 seconds
+
+    function getExtentionStorage() {
+        chrome.storage.local.get([null], (storage) => {
+            console.log(storage)
+        })
+    }
+
+    function setTabTitle() {
+        setTimeout(() => {
+            
+        }, 5000);
+    }
+
+    setTimeout(() => {
+        getExtentionStorage();
+    }, 3000);
+}, 1000);
