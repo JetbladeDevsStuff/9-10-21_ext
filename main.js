9ae551a8242abb13764d3f7ebcd7ebdb63a00aac
JetbladeDevsStuff <51022942+JetbladeDevsStuff@users.noreply.github.com>
2020-11-19 11:44:22 -0500

    Update main.js of BigSettings
    + Added functionality that replaces the text box's placeholder with the current value of the tabTitle

--- main.js
+++ main.js
@@ -1,60 +1,29 @@
-// main.js of 9 + 10 = 21
-var debug = true;
-if(debug) {
-  console.log("[9 + 10 = 21 DEBUG] debug enabled - expect console to be spammed a bit");
-}
-  // We will write a function that will check if the extention "on/off"
-  // state has been changed (passing null into the get function
-  // returns everything in storage)
-  async function getExtentionStorage(_callback) {
-      await chrome.storage.local.get(null, (data) => {
-          if (debug) {
-            console.log("[9 + 10 = 21 DEBUG] storage object:");
-            console.log(data);
-          }
-          _callback(data);
-      });
-  }
-
-async function setTitle(_callback) {
-  // https://stackoverflow.com/questions/13455134/javascript-doesnt-seem-to-wait-for-return-values
-  // i spent so much time searching for the solution to this
-  // callbacks are the best
-  await getExtentionStorage((data) => {
-    // This will actually do something
-    // TODO: Make data.on = true or false to make the code look cleaner
-    if (data.on == "on") {
-      document.querySelector("title").innerText = data.tabText;
-    } else {
-      if(debug){
-        console.log("[9 + 10 = 21 DEBUG] thingy is disabled");
-    }}
-    _callback();
-});
+// main.js of BigSettings for 9 + 10 = 21
+// Fuction to save to chrome's local storage
+function save() {
+    var option = document.getElementById("choice").value;
+    var text = document.getElementById("textBox").value;
+    chrome.storage.local.set({"on" : option, "tabText" : text}, () => {
+        // Runs when the action is complete, in this case we want to edit some
+        // text so that the user knows their changes were saved
+        var saveText = document.getElementById("saveText");
+        saveText.innerText = "Saved.";
+        // Now we will use setTimeout to clear the text after 3 seconds
+        setTimeout(() => {saveText.innerText = ''}, 3000);
+    });
 }
 
-document.addEventListener('DOMContentLoaded', setTitle(() => {}));
+function replacePlaceholderText() {
+  var placeholderBox = document.getElementById("textBox");
+  chrome.storage.local.get("tabText", (data) => {
+    if(data.tabText !== undefined) {
+      placeholderBox.setAttribute("placeholder",data.tabText);
+  }});
+}
 
-// mutation observers are wierd
-var title = document.querySelector("title");
-// observer config
-var oConfig = { childList: true, subtree: true };
-
-var observer = new MutationObserver((mutationsList) => {
-  console.log(mutationsList);
-  observer.disconnect();
-  if(debug) {
-    console.log("[9 + 10 = 21 DEBUG] about to change title - stopped observing");
-  }
-  setTitle(() => {
-    
-    setTimeout(() => {
-      observer.observe(title, oConfig);
-      if(debug) {
-        console.log("[9 + 10 = 21 DEBUG] 200ms passed - observing again");
-      }
-    }, 200);
-  });
+// Adds a listener to the button element for when it is clicked once the DOM
+// is loaded
+window.addEventListener('DOMContentLoaded', () => {
+  replacePlaceholderText();
+  document.getElementById('save').addEventListener('click', save);
 });
-
-observer.observe(title, oConfig);
