73f1a70f97325c51c22973fe8deeec71deb9b1fd
JetbladeDevsStuff <51022942+JetbladeDevsStuff@users.noreply.github.com>
2020-11-19 11:35:19 -0500

    Add the actual functionality
    + Added the functionality promised with callbacks

--- main.js
+++ main.js
@@ -1,16 +1,60 @@
 // main.js of 9 + 10 = 21
-// This whole thing will wait a half a second for jQuery to load
-setTimeout(() => {
-    var title = $("title").html();
-    // We will write a function that will check if the extention "on/off" 
-    // state has been changed (passing null into the get function 
-    // returns everything in storage)
-    function getExtentionStorage() {
-        chrome.storage.local.get(null, (data) => {
+var debug = true;
+if(debug) {
+  console.log("[9 + 10 = 21 DEBUG] debug enabled - expect console to be spammed a bit");
+}
+  // We will write a function that will check if the extention "on/off"
+  // state has been changed (passing null into the get function
+  // returns everything in storage)
+  async function getExtentionStorage(_callback) {
+      await chrome.storage.local.get(null, (data) => {
+          if (debug) {
+            console.log("[9 + 10 = 21 DEBUG] storage object:");
             console.log(data);
-            return(data.key);
-        })
-    }
+          }
+          _callback(data);
+      });
+  }
 
-    console.log(getExtentionStorage())
-}, 500);
+async function setTitle(_callback) {
+  // https://stackoverflow.com/questions/13455134/javascript-doesnt-seem-to-wait-for-return-values
+  // i spent so much time searching for the solution to this
+  // callbacks are the best
+  await getExtentionStorage((data) => {
+    // This will actually do something
+    // TODO: Make data.on = true or false to make the code look cleaner
+    if (data.on == "on") {
+      document.querySelector("title").innerText = data.tabText;
+    } else {
+      if(debug){
+        console.log("[9 + 10 = 21 DEBUG] thingy is disabled");
+    }}
+    _callback();
+});
+}
+
+document.addEventListener('DOMContentLoaded', setTitle(() => {}));
+
+// mutation observers are wierd
+var title = document.querySelector("title");
+// observer config
+var oConfig = { childList: true, subtree: true };
+
+var observer = new MutationObserver((mutationsList) => {
+  console.log(mutationsList);
+  observer.disconnect();
+  if(debug) {
+    console.log("[9 + 10 = 21 DEBUG] about to change title - stopped observing");
+  }
+  setTitle(() => {
+    
+    setTimeout(() => {
+      observer.observe(title, oConfig);
+      if(debug) {
+        console.log("[9 + 10 = 21 DEBUG] 200ms passed - observing again");
+      }
+    }, 200);
+  });
+});
+
+observer.observe(title, oConfig);
