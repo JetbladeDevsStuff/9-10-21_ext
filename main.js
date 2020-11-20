5561da3045a8f003ff420d32f226e12544959883
JetbladeDevsStuff <51022942+JetbladeDevsStuff@users.noreply.github.com>
2020-11-20 08:45:43 -0500

    Optimized - extension works amazingly now!
    + Optimized the extension

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 // main.js of 9 + 10 = 21
-var debug = true;
+var debug = false;
+
+getExtentionStorage((data) => {
+  if(data.debug == "on") {
+    debug = true;
+  } else {
+    debug = false;
+  }
+});
 if(debug) {
   console.log("[9 + 10 = 21 DEBUG] debug enabled - expect console to be spammed a bit");
 }
@@ -23,13 +31,16 @@ async function setTitle(_callback) {
   await getExtentionStorage((data) => {
     // This will actually do something
     // TODO: Make data.on = true or false to make the code look cleaner
-    if (data.on == "on") {
+    if (data.on == "on" || data.on === undefined) {
+      if(data.tabText == "") {
+        data.tabText = "9 + 10 = 21";
+      }
       document.querySelector("title").innerText = data.tabText;
     } else {
       if(debug){
         console.log("[9 + 10 = 21 DEBUG] thingy is disabled");
     }}
-    _callback();
+    _callback(data.tabText);
 });
 }
 
@@ -40,21 +51,47 @@ var title = document.querySelector("title");
 // observer config
 var oConfig = { childList: true, subtree: true };
 
-var observer = new MutationObserver((mutationsList) => {
-  console.log(mutationsList);
+var observer = new MutationObserver(mutationDetected);
+
+async function mutationDetected(mutationsList)
+{
+  if(debug) {
+    console.log("[9 + 10 = 21 DEBUG] mutationsList:");
+    console.log(mutationsList);
+  } else if (!mutationsList) {
+    if(debug) {console.log("[9 + 10 = 21 DEBUG] didn't get a mutationsList :(");}
+  }
   observer.disconnect();
+
   if(debug) {
     console.log("[9 + 10 = 21 DEBUG] about to change title - stopped observing");
   }
-  setTitle(() => {
+  setTitle((tabText) => {
     
-    setTimeout(() => {
+    setTimeout((tabTitle) => {
       observer.observe(title, oConfig);
       if(debug) {
         console.log("[9 + 10 = 21 DEBUG] 200ms passed - observing again");
       }
+      
+      if(title.innerText != tabText) {
+        if(debug) {
+          console.log("[9 + 10 = 21 DEBUG] missed a title (missed from mutationDetected)! reexecuting function");
+        }
+        mutationDetected();
+      }
     }, 200);
   });
-});
+}
 
 observer.observe(title, oConfig);
+if(debug) {console.log("[9 + 10 = 21 DEBUG] just started observing i think");}
+
+getExtentionStorage((data) => {
+  if(title.innerText != data.tabTitle) {
+    if(debug) {
+      console.log("[9 + 10 = 21 DEBUG] missed the title (1st time title didn't change)! executing mutationDetected()");
+    }
+    mutationDetected();
+  }
+});
